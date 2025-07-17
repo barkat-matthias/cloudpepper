b5bbd0ce3e2db726057319a99f944d2f0bb7f47e
Matthias Barkat <matthiasbarkat@foodles.co>
2025-07-17 06:54:06 +0200

    init

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+
+{
+    "name": "Cloudpepper test",
+    "summary": "",
+    "category": "",
+    "version": "14.0.1.0.0",
+    "author": "Foodles",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": [],
+    "application": True,
+}
+
---
+++ cloudpepper/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Cloudpepper test",
+    "summary": "",
+    "category": "",
+    "version": "14.0.1.0.0",
+    "author": "",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": []    "application": True,
+}
+
