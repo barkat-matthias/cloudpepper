db7eafc823d31c02f8172c03d945e96eee9ffaef
Matthias Barkat <matthiasbarkat@foodles.co>
2025-07-17 07:08:24 +0200

    init

--- cloudpepper/__manifest__.py
+++ cloudpepper/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Cloudpepper test",
     "summary": "",
     "category": "",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "",
     "website": "",
     "license": "AGPL-3",
